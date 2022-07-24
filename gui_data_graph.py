6e88895b2c898ae6a59d121ec171649cdcb410fe
ATakan Bıyıklı <atakanbiyikli@gmail.com>
2022-07-24 23:10:32 +0300

    işlevleri fonksiyonlara dağıt

--- gui_data_graph.py
+++ gui_data_graph.py
@@ -14,6 +14,12 @@ class MyApp(QDialog):
     def __init__(self):
         super(MyApp, self).__init__() 
         uic.loadUi('gui.ui', self) # Load the .ui file
+
+        self.ID =[]
+        self.angle =[] 
+        self.angle_ma = []
+        self.x = []
+        self.y = []
               
         self.pathButton.clicked.connect(self.getFilePath) # get path button         
         self.plotButton.clicked.connect(self.plot) # plot button
@@ -30,62 +36,57 @@ class MyApp(QDialog):
         )
         self.pathTextBox.setText(response[0])
         self.path = response[0]
-
-    def plot(self):
-        self.path = self.pathTextBox.text()
-        print(self.path)
-        
-        # init
-        ID =[]
-        angle =[] 
-        angle_ma = []
-        x = []
-        y = []
-
-        # read file
+    
+    def openFile(self):
         with open(self.path,"r") as csvfile:
             lines = csv.reader(csvfile, delimiter=',')
             for row in lines:
                 # ID_ = int(re.sub("[^0-9]", "", row[0]))
                 # if lines.line_num < 3: ID.append(ID_)
                 # else: ID.append(ID_ if  ID_ > (ID[lines.line_num - 3] + 50) else ID[lines.line_num - 3])
-                ID.append(int(re.sub("[^0-9]", "", row[0])))
-                angle.append(float(row[1]))
-                x.append(float(row[2]))
-                y.append(float(row[3]))
+                self.ID.append(int(re.sub("[^0-9]", "", row[0])))
+                self.angle.append(float(row[1]))
+                self.x.append(float(row[2]))
+                self.y.append(float(row[3]))
 
-        # moving average
-        if self.maCheckBox.isChecked():
-            # ma_frame_size = 40
-            ma_frame_size = int(self.maTextBox.text())
-            for i in  range(len(angle)):
-                if i < ma_frame_size:
-                    pass
-                else:
-                    angle_ma.append(numpy.sum(angle[i - ma_frame_size : i]) / ma_frame_size)
+    def movingAverage(self):        
+        ma_frame_size = int(self.maTextBox.text())
+        for i in  range(len(self.angle)):
+            if i < ma_frame_size:
+                pass
+            else:
+                self.angle_ma.append(numpy.sum(self.angle[i - ma_frame_size : i]) / ma_frame_size)
+
+
+    def plot(self):
+        self.path = self.pathTextBox.text()
+        print(self.path)
+        
+        self.openFile()
+        if self.maCheckBox.isChecked(): self.movingAverage()    
 
         # plots
         # figure 1
         plt.figure("All Results").canvas.manager.window.move(100,100)
         plt.subplot(4,1,1) 
-        plt.plot(range(len(ID)), ID)
+        plt.plot(range(len(self.ID)), self.ID)
         plt.ylabel('QR ID')
         plt.grid()
 
         plt.subplot(4,1,2) 
-        plt.plot(range(len(y)), y)
+        plt.plot(range(len(self.y)), self.y)
         plt.ylabel('Y Deviation(mm)')
         plt.grid()
 
         plt.subplot(4,1,3) 
-        plt.plot(range(len(x)), x)
+        plt.plot(range(len(self.x)), self.x)
         plt.ylabel('X Deviation(mm)')
         plt.grid()
 
         plt.subplot(4,1,4) 
-        plt.plot(range(len(angle)), angle, label="raw")
+        plt.plot(range(len(self.angle)), self.angle, label="raw")
         if self.maCheckBox.isChecked():
-            plt.plot(range(len(angle_ma)), angle_ma, label="moving average")
+            plt.plot(range(len(self.angle_ma)), self.angle_ma, label="moving average")
         plt.legend(loc="upper right")
         plt.ylabel('Angle(deg)')
         plt.grid()
@@ -95,8 +96,8 @@ class MyApp(QDialog):
 
         # figure 2
         plt.figure("QR ID vs Y deviation").canvas.manager.window.move(750,100)
-        plt.plot(range(len(ID)), ID,label="ID")
-        plt.plot(range(len(y)), y,label="Y")
+        plt.plot(range(len(self.ID)), self.ID,label="ID")
+        plt.plot(range(len(self.y)), self.y,label="Y")
         plt.legend(loc="upper right")
         plt.grid()
 
